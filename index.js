5e856e120852516d6738c74aaf9c0ce8179e9ee1
rajatbeladiya <rajatbeladiya7@gmail.com>
2019-11-25 22:49:54 +0530

    configure structure and user register api added

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// define middleware
+
+app.use(express.json({ extended: false }));
+
+// define routes
+
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 3000;
 
