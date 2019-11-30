6f0478cc75e34383803982a76062fa9cc8cb5fc8
rajatbeladiya <rajatbeladiya7@gmail.com>
2019-11-30 15:01:44 +0530

    authentication added, profile model and profile get api added

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -49,19 +51,27 @@ router.post(
       });
 
       const salt = await bcrypt.genSalt(10);
-
       user.password = await bcrypt.hash(password, salt);
-
       await user.save();
 
-      res.send('User registered.');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
 
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      )
     } catch (error) {
       res.status(500).send(error.message);
     }
-
-    console.log(req.body);
-    res.send('User Route')
 });
 
 module.exports = router;
