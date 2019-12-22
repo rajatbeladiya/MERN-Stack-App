aa8485458eb999a344abac9b8a8b347010c3cbb3
rajatbeladiya <rajatbeladiya7@gmail.com>
2019-12-22 16:20:39 +0530

    Finish profile actions & reducer

--- client-React-App/src/reducers/profile.js
+++ client-React-App/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE, GET_PROFILES, GET_REPOS } from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -18,6 +18,12 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false,
       }
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      }
     case PROFILE_ERROR:
       return {
         ...state,
@@ -28,6 +34,13 @@ export default function(state = initialState, action) {
       return {
         ...state,
         profile: null,
+        repos: [],
+        loading: false,
+      }
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         repos: [],
         loading: false,
       }
